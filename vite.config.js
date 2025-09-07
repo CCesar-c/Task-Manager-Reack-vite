05385f8affff3cc762225e6a74d502c095d05a63
CCesar-c <lianbastiam@gmail.com>
2025-09-07 14:35:05 -0300

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://CCesar-c.github.io/Task-Manager-Reack-vite/", // replace with your repo name
+  base: "/Task-Manager-Reack-vite/", // replace with your repo name
 });
